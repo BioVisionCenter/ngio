fb1f4b9d5dcc35ae4789f782c53c63718b236d78
lorenzo <lorenzo.cerrone@uzh.ch>
2025-10-06 09:06:01 +0200

    test chunk slice

--- tests/unit/io_pipes/test_slicing_ops.py
+++ tests/unit/io_pipes/test_slicing_ops.py
@@ -46,6 +46,7 @@ def test_slicing_ops_base(
         remove_channel_selection=False,
     )
     assert slicing_ops.slice_axes == tuple(output_axes)
+    assert slicing_ops.slice_chunks() == {(0,) * len(slicing_ops.on_disk_axes)}
 
     # Numpy
     zarr_data = zarr.zeros(shape=input_shape, chunks=input_shape, dtype="uint8")
@@ -58,3 +59,35 @@ def test_slicing_ops_base(
     data = get_slice_as_dask(zarr_data, slicing_ops=slicing_ops)
     assert data.shape == output_shape
     set_slice_as_dask(zarr_array=zarr_data, patch=data, slicing_ops=slicing_ops)
+
+
+def test_chunk_slice():
+    input_axes = "tczyx"
+    input_shape = (2, 3, 10, 11, 12)
+    chunk_shape = (1, 1, 10, 11, 12)
+    axes = [Axis(name=name) for name in input_axes]
+
+    ax_mapper = AxesHandler(axes=axes)
+    ds = Dataset(
+        path="0",
+        axes_handler=ax_mapper,
+        scale=[1.0] * len(axes),
+        translation=[0.0] * len(axes),
+    )
+    dims = Dimensions(shape=input_shape, chunks=chunk_shape, dataset=ds)
+
+    slicing_dict = {"t": 0, "c": (0, 2)}
+    slicing_ops = build_slicing_ops(
+        dimensions=dims,
+        slicing_dict=slicing_dict,  # type: ignore[arg-type]
+        remove_channel_selection=False,
+    )
+    assert slicing_ops.slice_chunks() == {(0, 0, 0, 0, 0), (0, 2, 0, 0, 0)}
+
+    slicing_dict = {"t": 1, "c": (0, 2)}
+    slicing_ops = build_slicing_ops(
+        dimensions=dims,
+        slicing_dict=slicing_dict,  # type: ignore[arg-type]
+        remove_channel_selection=False,
+    )
+    assert slicing_ops.slice_chunks() == {(1, 0, 0, 0, 0), (1, 2, 0, 0, 0)}
